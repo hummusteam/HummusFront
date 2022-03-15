eee1c427d12a5dd84dc72c94b8aad1a475d1e087
chandle greff <unconfigured@null.spigotmc.org>
2022-03-15 17:52:01 +0100

    fixed linking issue on menu page

--- src/api/MenuItemCategory.ts
+++ src/api/MenuItemCategory.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { MenuItem } from "../types";
 
-export default async function fetchItemByCategory(category: string) : Promise<MenuItem[]> {
+export default async function fetchItemByCategory(category: string | null) : Promise<MenuItem[]> {
     return await axios.get(`https://menuapi.tycho.dev/MenuItem/GetByCategory/${category}`).then(res => res.data).catch(console.log);
 }
--- src/components/SmallCategoryCard.tsx
+++ src/components/SmallCategoryCard.tsx
@@ -1,13 +1,12 @@
 import '../styles/CategoryCard.css'
-import { Link } from 'react-router-dom'
 import { Category } from '../types'
 
 export default function SmallCategoryCard(category: Category) {
   return (
-    <Link to={`/menu?category=${category.id}`}>
+    <a href={`/menu?category=${category.id}`}>
       <div className="category-card-container small-category-card" style={{ backgroundImage: `url(${category.image})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
         {category.name}
       </div>
-    </Link>
+    </a>
   )
 }
