4b23d3e718029c29c2c3344905f36856ce623e44
Borga <400446@student.fontys.nl>
2022-03-21 11:36:58 +0100

    Created future use all materials page

--- src/api/MenuItems.ts
+++ src/api/MenuItems.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { MenuItem } from "../types";
 
-export default async function fetchCategories() : Promise<MenuItem[]> {
+export default async function fetchItems() : Promise<MenuItem[]> {
     return await axios.get("https://menuapi.tycho.dev/MenuItem").then(res => res.data).catch(console.log);
 }
