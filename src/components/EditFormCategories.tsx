48816020c3cbbb4017d4821294b89a7387cb5cb0
Borga <400446@student.fontys.nl>
2022-03-22 13:29:54 +0100

    Changed name of css

--- src/components/EditFormCategories.tsx
+++ src/components/EditFormCategories.tsx
@@ -1,4 +1,4 @@
-import '../styles/EditableForm.css'
+import '../styles/EditForm.css'
 import { useState } from 'react'
 import { Category, Editable, MenuItem } from '../types'
 import Button from './Button'
