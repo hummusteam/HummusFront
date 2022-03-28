d4e54750252718cf9dc7c377f96f77c092af7e1e
chandle greff <unconfigured@null.spigotmc.org>
2022-03-28 15:22:48 +0200

    remove test page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,3 @@
 export { default as Home } from "./Home";
 export { default as Menu } from "./Menu";
 export { default as Ingredients } from "./Ingredients";
-export { default as Test } from "./Test";
