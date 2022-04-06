478b6b008a464c34c231e7011d61adeab1db4ae3
NOurkhatib <m.n.khatib2016@gmail.com>
2022-04-06 17:21:58 +0200

    Merge branch 'cart-features' of https://github.com/hummusteam/HummusFront into cart-features

--- src/components/CartCurrentOrder.tsx
+++ src/components/CartCurrentOrder.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+
 import '../styles/CurrentOrder.css'
 import Select from 'react-select'
-import { Component } from 'react'
+import Cookies from 'universal-cookie'
+import { MenuItem } from '../types'
+import MenuItemDetails from './MenuItemDetails'
+import OrderItem from '../types/OrderItem'
+import { useState } from 'react'
+import Order from '../types/Order'
+
 
 const options = [
     { value: 'chocolate', label: 'Chocolate' },
@@ -9,40 +15,65 @@ const options = [
     { value: 'vanilla', label: 'Vanilla' },
 ]
 
+
+// const menuItem = 0
+// const [menuItems, setMenuItem] = useState<MenuItem[]>([])
+
+// useEffect(() => {
+//     fetchMenuItemsByCategory(categoryId).then(setMenuItem)
+//     fetchCategories().then(setCategories)
+//   }, [])
+
+// const [fullOrders, setfullOrders] = useState<Order>()
+
+const cookies = new Cookies()
+if (cookies.get('_order')) {
+    // Deserialize object from cookie using casting
+    const order: OrderItem[] = cookies.get('_order')
+
+    // cookies.set('_order', JSON.stringify(order))
+    // console.log(order)
+    console.log("NOUROOOOOOOOOOOOOOO")
+    console.log(order[1].extraIngredients)
+}
+
 export default function CartCurrentOrder() {
     return (
         <>
-            <div>
-                <div className="orders-container">
 
-                    <div className="generic">
-                        <div className='my-orders'>
+            <div className="orders-container">
+                <div className="generic">
+                    <div className='my-orders'>
 
-                        </div>
-                    </div>
-                    <div className="generic">
-                        <div className="previous-orders">
-                            <Select options={options} />
-                        </div>
-                    </div>
-                    <div className="generic">
-                        <div className="notes">
-                            <input className="e-input" type="text" placeholder="Notes" />
-                        </div>
-                    </div>
-                    <div className="generic">
-                        <div className="Botton">
-                            <button type="submit" className="btn">
-                                Place order
-                            </button>
-                            <button type="button" className="btn">
-                                Payment
-                            </button>
-
-                        </div>
                     </div>
                 </div>
             </div>
+            <div className="generic">
+                <div className="previous-orders">
+                    <Select options={options} />
+                </div>
+            </div>
+
+            <div className="generic">
+                <div className="notes">
+                    <input className="e-input" type="text" placeholder="Notes" />
+                </div>
+            </div>
+
+            <div className="generic">
+                <div className="Botton">
+                    <button type="submit" className="btn">
+                        Place order
+                    </button>
+
+                    <button type="button" className="btn">
+                        Payment
+                    </button>
+                </div>
+            </div>
+
         </>
     )
 }
+
+
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,31 +1,15 @@
-import '../styles/Home.css'
+// import '../styles/Home.css'
 import { CartCurrentOrder, AddFormCategory, CategoryCard, Loading, Navigation } from '../components'
 import { useState, useEffect } from 'react'
-import { fetchCategories } from '../api'
-import { Category } from '../types'
+import { fetchCategories, fetchMenuItemsByCategory } from '../api'
+import { Category, MenuItem } from '../types'
+import { useSearchParams } from 'react-router-dom'
 
-const AUTHED = false
 
 export default function Cart() {
-    const banner = 'https://www.nestleprofessionalmena.com/sites/default/files/2020-05/Vision%20banner.png'
-    const [categories, setCategories] = useState<Category[]>([])
-
-    useEffect(() => {
-        fetchCategories().then(setCategories)
-    }, [])
-
     return (
         <>
-            {categories.length != 0 ? (
-                <div className="app-container">
-                    <Navigation url={banner} />
-                    <CartCurrentOrder />
-
-
-                </div>
-            ) : (
-                <Loading />
-            )}
+            {<CartCurrentOrder />}
         </>
     )
 }
