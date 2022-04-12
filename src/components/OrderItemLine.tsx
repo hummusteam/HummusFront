48faa0062a9cc1b4e4fe264f22520e0469d1f568
chandle greff <unconfigured@null.spigotmc.org>
2022-04-12 13:27:12 +0200

    Simplified order line comp

--- src/components/OrderItemLine.tsx
+++ src/components/OrderItemLine.tsx
@@ -1,17 +1,22 @@
 import { useEffect, useState } from 'react'
 import { fetchIngredientById } from '../api'
 import '../styles/OrderItemLine.css'
-import { MenuItem, OrderItem } from '../types'
+import { Ingredient, MenuItem, OrderItem } from '../types'
 import Button from './Button'
 
+type IngredientElement = {
+  ingredient: Ingredient
+  qty: number
+}
+
 export default function OrderItemLine({ menuItem, orderItem }: { menuItem: MenuItem; orderItem: OrderItem }) {
-  const [extras, setExtras] = useState<MenuItem[]>([])
+  const [extras, setExtras] = useState<IngredientElement[]>([])
   const qty = 1
 
   useEffect(() => {
     Object.entries(orderItem.extraIngredients).map(async (item) => {
       fetchIngredientById(item[0]).then((data) => {
-        setExtras((varr) => [...varr, data])
+        setExtras((varr) => [...varr, { ingredient: data, qty: item[1] }])
       })
     })
   }, [])
@@ -20,26 +25,24 @@ export default function OrderItemLine({ menuItem, orderItem }: { menuItem: MenuI
     <div className="orderlineContainer">
       <div className="orderlineDetails">
         <div className="orderlineHeader">
-          <h3>
+          <h2>
             {qty} x {menuItem.name}
-          </h3>
-          <h3>
+          </h2>
+          <h2>
             {menuItem.price * qty} <small>€</small>
-          </h3>
+          </h2>
         </div>
-        {extras.length != 0 ? (
+        {extras && extras.length != 0 ? (
           <div className="extrasOrderline">
             <b>Extras ingredients</b>
             {extras.map((i) => {
-              return Object.entries(orderItem.extraIngredients).map((o) => {
-                return o[0] == i.id ? (
-                  <div key={i.id} className="ingredientItemLine">
-                    <div className="ingredientDetailsLine">
-                      {o[1]} x {i.name}
-                    </div>
+              return (
+                <div key={i.ingredient.id} className="ingredientItemLine">
+                  <div className="ingredientDetailsLine">
+                    {i.qty} x {i.ingredient.name}
                   </div>
-                ) : null
-              })
+                </div>
+              )
             })}
           </div>
         ) : null}
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,47 +1,64 @@
+import { useRouter } from 'next/router'
 import { useState, useEffect } from 'react'
 import Cookies from 'universal-cookie'
 import { fetchMenuItemByID } from '../api/MenuItems'
-import { Navigation, OrderItemLine } from '../components'
+import { Button, Navigation, OrderItemLine } from '../components'
 import { MenuItem, OrderItem } from '../types'
+
+type OrderItemElement = {
+  orderItem: OrderItem
+  menuItem: MenuItem
+  qty: number
+}
 
 export default function Cart() {
   const banner = 'https://www.nestleprofessionalmena.com/sites/default/files/2020-05/Vision%20banner.png'
-
-  const [menuItems, setMenuItems] = useState<MenuItem[]>([])
-  const [orderItems, setOrderItems] = useState<OrderItem[]>([])
+  const [orderItemElements, setOrderItemElements] = useState<OrderItemElement[]>([])
+  // const router = useRouter()
 
   useEffect(() => {
-    let cartCookies = new Cookies()
+    const cartCookies = new Cookies()
 
     if (cartCookies.get('_order')) {
-      const storedFullOrder: OrderItem[] = cartCookies.get('_order')
+      const orderItems: OrderItem[] = cartCookies.get('_order')
 
-      setOrderItems(storedFullOrder)
-
-      for (let i = 0; i < storedFullOrder.length; i++) {
-        fetchMenuItemByID(storedFullOrder[i].menuItemId).then((data) => {
-          setMenuItems((varr) => [...varr, data])
+      orderItems.forEach((orderItem) => {
+        fetchMenuItemByID(orderItem.menuItemId).then((data) => {
+          setOrderItemElements((varr) => [...varr, { orderItem: orderItem, menuItem: data, qty: 1 }])
         })
-      }
+      })
     }
   }, [])
+
+  function handleOrderMore() {
+    // router.push('/')
+  }
+
+  function handleCancelation() {
+    window.alert('cancel order')
+  }
 
   return (
     <div className="app-container">
       <Navigation url={banner} />
 
+      <div className="cartRedirectBtns">
+        <div onClick={handleOrderMore}>
+          <Button text="Order more" />
+        </div>
+        <div onClick={handleCancelation}>
+          <Button text="Cancel my order" />
+        </div>
+      </div>
+
       <div className="ordersContainer">
         <div className="orderBoxContainer">
           <h1>Cart Overview</h1>
           <div className="orderItems">
-            {orderItems.length != 0 &&
-              menuItems.length != 0 &&
-              orderItems.map((orderItem) => {
-                for (let i = 0; i < menuItems.length; i++) {
-                  if (orderItem.menuItemId === menuItems[i].id) {
-                    return <OrderItemLine key={orderItem.id} menuItem={menuItems[i]} orderItem={orderItem} />
-                  }
-                }
+            {orderItemElements &&
+              orderItemElements.length != 0 &&
+              orderItemElements.map((orderItemElement) => {
+                return <OrderItemLine key={orderItemElement.orderItem.id} menuItem={orderItemElement.menuItem} orderItem={orderItemElement.orderItem} />
               })}
           </div>
           <div className="orderOverview">
