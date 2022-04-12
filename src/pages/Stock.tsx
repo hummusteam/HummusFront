2053c47be65aaf7fc7c0b7b4aa65429ca390cc3d
chandle greff <unconfigured@null.spigotmc.org>
2022-04-12 09:41:20 +0200

    Renamed comps

--- src/pages/Stock.tsx
+++ src/pages/Stock.tsx
@@ -1,24 +1,13 @@
 import '../styles/Stock.css'
 import { Navigation, StockTable } from '../components'
-import { useState, useEffect } from 'react'
-import { fetchIngredients } from '../api'
-import { Ingredient } from '../types'
 
-
-
-export default function Cart() {
-  
-  const [ingredients, setIngredients] = useState<Ingredient[]>([])
-  
-  useEffect(() => {
-    fetchIngredients().then(setIngredients)
-  }, [])
-  
+export default function Stock() {
   const banner = 'https://www.nestleprofessionalmena.com/sites/default/files/2020-05/Vision%20banner.png'
+
   return (
     <div className="app-container ">
       <Navigation url={banner} />
-        <StockTable />
+      <StockTable />
     </div>
   )
 }
