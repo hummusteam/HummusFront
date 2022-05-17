09b52d2a80d1672fab4c938c67d4934dd958cdd0
chandle greff <unconfigured@null.spigotmc.org>
2022-05-17 14:06:41 +0200

    Added update order

--- src/api/Order.ts
+++ src/api/Order.ts
@@ -8,6 +8,13 @@ export async function placeOrder(sessionId: string, order: Order) {
     .catch(console.log)
 }
 
-export async function fetchAllOrders() : Promise<Order[]> {
-  return await axios.get('https://orderapi.tycho.dev/Order').then(res => res.data)
+export async function fetchAllOrders(): Promise<Order[]> {
+  return await axios.get('https://orderapi.tycho.dev/Order').then((res) => res.data)
+}
+
+export async function updateOrder(order: Order) {
+  return await axios
+    .put('https://orderapi.tycho.dev/Order', order)
+    .then((res) => res.data)
+    .catch(console.log)
 }
--- src/api/index.ts
+++ src/api/index.ts
@@ -3,4 +3,4 @@ export { fetchMenuItems, fetchMenuItemById, fetchMenuItemsByCategory, putMenuIte
 export { fetchIngredients, fetchIngredientById, putIngredient, postIngredient, deleteIngredient } from './Ingredients'
 export { startSession, fetchSessionOrders } from './Sessions'
 export { redirectToPaymnetWithId } from './Payment'
-export { placeOrder, fetchAllOrders } from './Order'
+export { placeOrder, updateOrder, fetchAllOrders } from './Order'
